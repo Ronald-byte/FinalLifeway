2f0a8470bc5388530631e700cc7e6a7d4a23f80c
USER <USER@DESKTOP-UUU898Q>
2020-05-05 22:02:18 -0500

    Delete

--- src/main/java/pe/edu/upc/serviceinterface/IDetail_rental.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.serviceinterface;
-
-import java.util.List;
-
-import pe.edu.upc.entity.Detail_rental;
-
-public interface IDetail_rental {
-	public List<Detail_rental> listar();
-}
