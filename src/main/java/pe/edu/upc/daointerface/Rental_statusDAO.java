e05143457a0b9d831ba21974d899004446af956c
jefhe <jefhe@DESKTOP-5CEV1DS>
2020-05-05 22:37:13 -0500

    Eliminacion de tablas innecesarias

--- src/main/java/pe/edu/upc/daointerface/Rental_statusDAO.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.daointerface;
-
-import java.util.List;
-
-import pe.edu.upc.entity.Rental_status;
-
-public interface Rental_statusDAO {
-	public List<Rental_status> listar();
-}
--- src/main/java/pe/edu/upc/serviceinterface/IRental_status.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.serviceinterface;
-
-import java.util.List;
-
-import pe.edu.upc.entity.Rental_status;
-
-public interface IRental_status {
-	public List<Rental_status> listar();
-}
